1111efbf90f5ebe9ae30f34c05e35de267326100
Maxim Fedorenko <varlllog@gmail.com>
2015-05-13 17:29:51 +0300

    Changes in init.sh script

--- init.sh
+++ init.sh
@@ -3,9 +3,9 @@
 CI_DIR='/ci'
 PROJECT_DIR='/project'
 
-"$CI_DIR/cppcheck-run.sh $PROJECT_DIR" || exit 1
-"$CI_DIR/vera-run.sh $PROJECT_DIR" || exit 2
-"$CI_DIR/rats-run.sh $PROJECT_DIR" || exit 3
+"$CI_DIR/cppcheck-run.sh" "$PROJECT_DIR" || exit 1
+"$CI_DIR/vera-run.sh" "$PROJECT_DIR" || exit 2
+"$CI_DIR/rats-run.sh" "$PROJECT_DIR" || exit 3
 
 exit 0
 
